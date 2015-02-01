ea780a97aa5bc9a23a1f3fed7a890a18584faa95
William Willing <william@williamwilling.com>
2015-02-01 12:40:03 +0100

    Added BasicController sample.

---
+++ src/AspNetMvc.BasicController/Controllers/GreetingsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.BasicController
+{
+    public class GreetingsController : Controller
+    {
+        public string Hello()
+        {
+            return "<html><body><p>Hello, world!</p></body></html>";
+        }
+    }
+}

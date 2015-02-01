53b2bfe432109e6ec3c8787df96ea26a75496dda
William Willing <william@williamwilling.com>
2015-02-01 13:07:47 +0100

    Added LayoutView sample.

---
+++ src/AspNetMvc.LayoutView/Controllers/GreetingsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.LayoutView
+{
+    public class GreetingsController : Controller
+    {
+        public ActionResult Hello()
+        {
+            return View();
+        }
+
+        public ActionResult Goodbye()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/AspNetMvc.LayoutView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvc.LayoutView.MvcApplication" Language="C#" %>

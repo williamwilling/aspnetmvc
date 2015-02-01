bf9ad08905ccac494fea9f95070d4857886e8a21
William Willing <william@williamwilling.com>
2015-02-01 12:47:23 +0100

    Added BasicView sample.

---
+++ src/AspNetMvc.BasicView/Controllers/GreetingsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.BasicView
+{
+    public class GreetingsController : Controller
+    {
+        public ActionResult Hello()
+        {
+            return View();
+        }
+    }
+}

3dd0e3e6dea1d6864f606c987b0ca572504640d0
William Willing <william@williamwilling.com>
2015-02-02 11:49:42 +0100

    Added ViewModel sample.

---
+++ src/AspNetMvc.ViewModel/Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.ViewModel
+{
+    public class GamesController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(GameViewModel game)
+        {
+            // NOTE: Put a breakpoint on the next line and inspect the properties of the parameter.
+            return View();
+        }
+    }
+}
---
+++ src/AspNetMvc.ViewModel/ViewModels/GameViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetMvc.ViewModel
+{
+    public class GameViewModel
+    {
+        public string PlayerOne { get; set; }
+        public string PlayerTwo { get; set; }
+        public int Rounds { get; set; }
+        public string Ruleset { get; set; }
+    }
+}

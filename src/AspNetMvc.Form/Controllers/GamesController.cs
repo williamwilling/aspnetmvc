ef456a5a8cf6c14f4d7e2049c2bcbd5511680a65
William Willing <william@williamwilling.com>
2015-02-02 11:41:25 +0100

    Added the Form sample.

---
+++ src/AspNetMvc.Form/Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.Form
+{
+    public class GamesController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string playerOne, string playerTwo, int rounds, string ruleset)
+        {
+            // NOTE: Put a breakpoint on the next line and inspect the values of the parameters.
+            return View();
+        }
+    }
+}

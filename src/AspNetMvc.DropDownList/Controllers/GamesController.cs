440e05adcabb5180095f3303bbcb40aa0f3aef6b
William Willing <william@williamwilling.com>
2015-02-03 12:14:27 +0100

    Added DropDownList sample.

---
+++ src/AspNetMvc.DropDownList/Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.DropDownList
+{
+    public class GamesController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View(new GameViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(GameViewModel game)
+        {
+            // NOTE: Put a breakpoint on the next line and inspect the properties of the parameter.
+            return View(new GameViewModel());
+        }
+    }
+}
---
+++ src/AspNetMvc.DropDownList/ViewModels/GameViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace AspNetMvc.DropDownList
+{
+    public class GameViewModel
+    {
+        [Required(ErrorMessage = "You have to enter a name for player one.")]
+        [DisplayName("Player one:")]
+        public string PlayerOne { get; set; }
+
+        [Required(ErrorMessage = "You have to enter a name for player two.")]
+        [DisplayName("Player two:")]
+        public string PlayerTwo { get; set; }
+
+        [Required(ErrorMessage = "You have to specify the number of rounds.")]
+        [Range(1, 7, ErrorMessage = "The number of rounds you specified is invalid. Minimum is 1, maximum is 7.")]
+        [DisplayName("Rounds:")]
+        public int Rounds { get; set; }
+
+        public string Ruleset { get; set; }
+
+        public IEnumerable<SelectListItem> Rulesets
+        {
+            get
+            {
+                return new SelectListItem[]
+                {
+                    new SelectListItem
+                    {
+                        Text = "Classic",
+                        Value = "classic"
+                    },
+
+                    new SelectListItem
+                    {
+                        Text = "Modern",
+                        Value = "modern"
+                    },
+
+                    new SelectListItem
+                    {
+                        Text = "None",
+                        Value = "anarchy"
+                    }
+                };
+            }
+        }
+    }
+}
